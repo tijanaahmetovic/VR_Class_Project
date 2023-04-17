be26dbbc119cc38bfe437993b7b583a8866f64c8
tijanaahmetovic <48787837+tijanaahmetovic@users.noreply.github.com>
2023-04-17 12:38:41 +0200

    Initial VR Project

---
+++ Assets/WalkingToRandomPosition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingToRandomPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RePositionWithDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator RePositionWithDelay(){
+        while(true){
+            SetRandomPosition();
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    void SetRandomPosition(){
+        float x = Random.Range(-5.0f, 5.0f);
+        float z = Random.Range(-5.0f, 5.0f);
+        Debug.Log("x, z: "+x.ToString("F2")+", "+z.ToString("F2"));
+        transform.position = new Vector3(x, 0.0f, z);
+    }
+}
